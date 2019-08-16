128199b2ac248f9cab4665a426b9d6260645aaf7
moushao <moushao1990@gmail.com>
2019-08-16 10:15:08 +0800

    change setting for project

--- src/main/java/com/mous/im/ImServerApplication.java
+++ src/main/java/com/mous/im/ImServerApplication.java
@@ -3,15 +3,12 @@ package com.mous.im;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import main.com.im.netty.IMServer;
-
 
 @SpringBootApplication
 public class ImServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ImServerApplication.class, args);
-        IMServer.getInstance().init();
     }
 
 }
